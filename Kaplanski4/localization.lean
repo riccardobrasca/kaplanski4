e1e17f9c2d8251e4cc6394a30838f5c3ca559604
Emilie Uthaiwat <emiliepathum@gmail.com>
2023-05-25 22:35:59 +0200

    long lines

--- Kaplanski4/localization.lean
+++ Kaplanski4/localization.lean
@@ -1,13 +1,17 @@
 import Kaplanski4.Kaplanski
 import Mathlib.RingTheory.Localization.Ideal
 
-lemma is_localization.prime_of_prime {R S : Type _} [CommRing R] [CommRing S] [Algebra R S] [IsDomain R] {M : Submonoid R} [IsLocalization M S] (hM : M ≤ nonZeroDivisors R) (p : R) (hp : Prime p) (hp₂ : ∃ (I : Ideal R),
-  Disjoint (M : Set R) (I : Set R) ∧ p ∈ I) : Prime ((algebraMap R S) p) := by
+lemma is_localization.prime_of_prime {R S : Type _} [CommRing R] [CommRing S] [Algebra R S]
+    [IsDomain R] {M : Submonoid R} [IsLocalization M S] (hM : M ≤ nonZeroDivisors R) (p : R)
+    (hp : Prime p) (hp₂ : ∃ (I : Ideal R), Disjoint (M : Set R) (I : Set R) ∧ p ∈ I) :
+    Prime ((algebraMap R S) p) := by
   rcases hp₂ with ⟨I, hI, hp₂⟩
-  have hp' := IsLocalization.isPrime_of_isPrime_disjoint M S (Ideal.span {p}) ((Ideal.span_singleton_prime hp.ne_zero).2 hp)
-    (Set.disjoint_of_subset_right (I.span_singleton_le_iff_mem.2 hp₂) hI)
+  have hp' := IsLocalization.isPrime_of_isPrime_disjoint M S (Ideal.span {p})
+    ((Ideal.span_singleton_prime hp.ne_zero).2 hp)
+      (Set.disjoint_of_subset_right (I.span_singleton_le_iff_mem.2 hp₂) hI)
   rw [Ideal.map_span, Set.image_singleton] at hp'
-  exact (Ideal.span_singleton_prime (fun hp₃ => hp.ne_zero ((IsLocalization.to_map_eq_zero_iff S hM).1 hp₃))).1 hp'
+  exact (Ideal.span_singleton_prime (fun hp₃ => hp.ne_zero
+    ((IsLocalization.to_map_eq_zero_iff S hM).1 hp₃))).1 hp'
 
 example {R S : Type _} [CommRing R] [CommRing S] [Algebra R S] [IsDomain R] {M : Submonoid R}
   [IsLocalization M S] (hM : M ≤ nonZeroDivisors R) [UniqueFactorizationMonoid R] :
@@ -23,4 +27,5 @@ example {R S : Type _} [CommRing R] [CommRing S] [Algebra R S] [IsDomain R] {M :
     rw [← IsLocalization.map_comap M S J, ← Idef, h, Ideal.map_bot] }
   obtain ⟨p, hpI, hp⟩ := exists_prime_of_uniqueFactorizationMonoid _ hI hIprime
   refine' ⟨algebraMap R S p, Ideal.mem_comap.mp hpI, _⟩
-  exact is_localization.prime_of_prime hM p hp ⟨I, ⟨(((IsLocalization.isPrime_iff_isPrime_disjoint M S J).1 hJprime).2), hpI⟩⟩
+  exact is_localization.prime_of_prime hM p hp ⟨I,
+    ⟨(((IsLocalization.isPrime_iff_isPrime_disjoint M S J).1 hJprime).2), hpI⟩⟩
